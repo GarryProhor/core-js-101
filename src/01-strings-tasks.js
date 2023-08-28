cb903b29006774d28c3e925b696462f66d4bcdaa
igorp <igorprohorchenko@gmail.com>
2023-08-28 13:07:50 +0300

    getCardId

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -325,8 +325,18 @@ function isString(value) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const deck = [
+    'A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
+    'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
+    'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
+    'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠',
+  ];
+
+  // Используем метод indexOf() для поиска индекса карты в массиве
+  const index = deck.indexOf(value);
+
+  return index;
 }
 
 
